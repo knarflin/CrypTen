977698ccb357cf7fb4a40478622f0a772bb0425a
Frank Lin <knarf.lin@yahoo.com>
2021-06-23 10:19:10 +0800

    Fix warning of non-writable numpy while reading onnx model (2)

--- crypten/nn/onnx_converter.py
+++ crypten/nn/onnx_converter.py
@@ -295,7 +295,7 @@ class FromOnnx:
         elif attr.HasField("s"):  # string attribute
             return attr.s  # TODO: Sanitize string.
         elif attr.HasField("t"):  # tensor attribute
-            return torch.from_numpy(numpy_helper.to_array(attr.t))
+            return torch.from_numpy(numpy.copy(numpy_helper.to_array(attr.t)))
         elif len(attr.ints) > 0:
             return list(attr.ints)
         elif len(attr.floats) > 0:

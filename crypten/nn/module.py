efed2ab7d27f849fce00ffaf7ec32a5f3489c4dc
Frank Lin <knarf.lin@yahoo.com>
2021-06-26 15:33:00 +0800

    Fix Bug: add () at the end of comm.get().get_rank

--- crypten/nn/module.py
+++ crypten/nn/module.py
@@ -666,7 +666,7 @@ class Graph(Container):
                     output = self._modules[node_to_compute](input)
                 t2 = time.time()
                 from crypten import communicator as comm
-                if comm.get().get_rank == 0:
+                if comm.get().get_rank() == 0:
                     print(f"{node_to_compute}, {self._modules[node_to_compute]}: {repeat_time} in {t2-t1}s, avg = {(t2-t1)/repeat_time}s",)
             else:
                 output = self._modules[node_to_compute](input)
